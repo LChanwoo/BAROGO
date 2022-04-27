7b730c425a417fc223ee961c163443bb21e68c34
LChanwoo <chw210601@gmail.com>
2022-04-27 23:10:08 +0900

    통합1

--- multiproject/src/main/java/edu/multi/kdigital/MultiprojectApplication.java
+++ multiproject/src/main/java/edu/multi/kdigital/MultiprojectApplication.java
@@ -9,6 +9,9 @@ import edu.multi.kdigital.controller.LoginController;
 import edu.multi.kdigital.controller.MainController;
 import edu.multi.kdigital.dao.PaymentMapper;
 import hotel.HotelDAO;
+import market.MarketController;
+import market.MarketDAO;
+import market.MarketadminController;
 import hotel.HotelController;
 import upload.UploadController;
 
@@ -17,9 +20,11 @@ import upload.UploadController;
 @ComponentScan(basePackageClasses = MainController.class)
 @ComponentScan(basePackageClasses = LoginController.class)
 @ComponentScan(basePackageClasses = HotelController.class)
+@ComponentScan(basePackageClasses = MarketadminController.class)
 @ComponentScan(basePackageClasses = UploadController.class)
 @MapperScan(basePackageClasses = HotelDAO.class)
 @MapperScan(basePackageClasses = PaymentMapper.class)
+@MapperScan(basePackageClasses = MarketDAO.class)
 public class MultiprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MultiprojectApplication.class, args);
