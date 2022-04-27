9a972c2ae445828c77ae5054759733b6a103613a
LChanwoo <chw210601@gmail.com>
2022-04-27 16:49:52 +0900

    찬우승현통합2차

--- multiproject/src/main/java/edu/multi/kdigital/MultiprojectApplication.java
+++ multiproject/src/main/java/edu/multi/kdigital/MultiprojectApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import edu.multi.kdigital.controller.LoginController;
+import edu.multi.kdigital.controller.MainController;
 import edu.multi.kdigital.dao.PaymentMapper;
 import hotel.HotelDAO;
 import hotel.HotelController;
@@ -12,6 +14,8 @@ import upload.UploadController;
 
 @SpringBootApplication
 @ComponentScan
+@ComponentScan(basePackageClasses = MainController.class)
+@ComponentScan(basePackageClasses = LoginController.class)
 @ComponentScan(basePackageClasses = HotelController.class)
 @ComponentScan(basePackageClasses = UploadController.class)
 @MapperScan(basePackageClasses = HotelDAO.class)
